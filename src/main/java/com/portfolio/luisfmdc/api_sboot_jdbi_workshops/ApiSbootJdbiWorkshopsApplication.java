a0c7ba0f057f08c0213d379af8e602bbb6a200ff
luisfmaiadc <lf.mcosta23@gmail.com>
2025-05-24 17:59:57 -0300

    Inicialização do projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/api_sboot_jdbi_workshops/ApiSbootJdbiWorkshopsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_jdbi_workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSbootJdbiWorkshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSbootJdbiWorkshopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/api_sboot_jdbi_workshops/ApiSbootJdbiWorkshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.api_sboot_jdbi_workshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSbootJdbiWorkshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
